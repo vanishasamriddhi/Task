cd5f44d68d56d9e623de0d42c01b80ba26db2fc6
Vanisha Samriddhi <71593673+vanishasamriddhi@users.noreply.github.com>
2021-01-26 16:24:44 +0530

    Create Operators1Question8.cpp

---
+++ Operators1Question8.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+ using namespace std;
+int main()
+{
+ int a=78, b=45, c=62;
+ cout<<"Robert total marks is : "<< a+b+c<<endl;
+	cout<<"Robert total percent is : "<< (a+b+c)/3<<endl;
+	return 0;
+ } 
