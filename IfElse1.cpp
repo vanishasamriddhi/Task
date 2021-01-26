62202bedee24853d591195e427643fda4510db60
Vanisha Samriddhi <71593673+vanishasamriddhi@users.noreply.github.com>
2021-01-26 16:39:03 +0530

    Create IfElse1.cpp

---
+++ IfElse1.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+ using namespace std;
+ 
+ int main()
+ {
+ 	int x=2,y=5,z=0;
+ 	if(x==2)
+              cout<<"true"<<endl;
+     else
+              cout<<"false"<<endl;
+     if(x!=5)
+              cout<<"true"<<endl;
+     else
+              cout<<"false"<<endl;
+     if( x != 5 && y >= 5)
+              cout<<"true"<<endl;
+     else
+              cout<<"false"<<endl;
+     if(z != 0 || x == 2)
+               cout<<"true"<<endl;
+     else
+               cout<<"false"<<endl;
+   if(!(y < 10))
+               cout<<"true"<<endl;
+     else
+               cout<<"false"<<endl;
+ 
+   return 0;
+ }
+ 	
