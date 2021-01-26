b85d0bfcc4ca426f643cc0048ed9783dc0bd61dd
Vanisha Samriddhi <71593673+vanishasamriddhi@users.noreply.github.com>
2021-01-26 16:40:24 +0530

    Create IfElse2.cpp

---
+++ IfElse2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+ using namespace std;
+ int main()
+ {
+ 	char c;
+ 	cin>>c;
+ 	if(islower(c)>0)
+ 	cout<<"THIS is in lower case"<<endl;
+ 	else
+ 	cout<<"this is in uppercase"<<endl;
+ 	return 0;
+ }
