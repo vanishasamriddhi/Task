f83776079d5138b794d90782d49fb8c683bd6b0b
Vanisha Samriddhi <71593673+vanishasamriddhi@users.noreply.github.com>
2021-01-26 16:12:48 +0530

    Create VariablesAndLiterals1.cpp

---
+++ VariablesAndLiterals1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+ using namespace std;
+ int main()
+ {
+ 	float a,b;
+ 	cout<<"Length of the Rectangle:"<<endl;
+ 	cin>>a;
+ 	cout<<"Breadth of the Rectangle:"<<endl;
+ 	cin>>b;
+ 	cout<<"Area of the Rectangle:"<< int( a*b )<<endl;
+ 	return 0;
+ 	
+ 	
+ }
